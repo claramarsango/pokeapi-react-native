114e4f410a6d513cc1ccb9cc3a55d1fbeead5375
claramarsango <claraxmarsango@gmail.com>
2023-06-06 16:51:00 +0200

    fix spacing formatting errors

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 /*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
+For a detailed explanation regarding each configuration property and type check, visit:
+https://jestjs.io/docs/configuration
+*/
 
 export default {
   // All imported modules in your tests should be mocked automatically
